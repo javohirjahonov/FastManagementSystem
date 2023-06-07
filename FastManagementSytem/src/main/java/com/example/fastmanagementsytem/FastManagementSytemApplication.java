139243d5e6bc47ee4c9087c131c5688db3ca70e4
Javokhir  Jakhonov <84447794+javohirjahonov@users.noreply.github.com>
2023-06-07 17:00:36 +0500

    Add files via upload

---
+++ FastManagementSytem/src/main/java/com/example/fastmanagementsytem/FastManagementSytemApplication.java
@@ -0,0 +1,13 @@
+package com.example.fastmanagementsytem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastManagementSytemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FastManagementSytemApplication.class, args);
+    }
+
+}
---
+++ FastManagementSytem/src/test/java/com/example/fastmanagementsytem/FastManagementSytemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fastmanagementsytem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FastManagementSytemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
