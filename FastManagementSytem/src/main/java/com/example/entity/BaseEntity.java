9b0b5fbf52849c170621f06ed8ef30b318cf5c32
Msi Laptop <hh@gg.com>
2023-06-08 13:49:02 +0500

    Added Base Entity

--- FastManagementSytem/src/main/java/com/example/entity/BaseEntity.java
+++ FastManagementSytem/src/main/java/com/example/entity/BaseEntity.java
@@ -19,7 +19,7 @@ import java.util.UUID;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
-public class BaseEntity {
+public  abstract class BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     protected UUID id;
