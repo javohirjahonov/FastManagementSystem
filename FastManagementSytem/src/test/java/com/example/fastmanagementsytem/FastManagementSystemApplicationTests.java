8ef08fad979e95508bb4ecb596147ac8d5d0c89f
ermamatovsardor <ermamatovsardor0@gmail.com>
2023-06-08 12:58:12 +0500

    BaseEntity created

--- FastManagementSytem/src/test/java/com/example/fastmanagementsytem/FastManagementSystemApplicationTests.java
+++ FastManagementSytem/src/test/java/com/example/fastmanagementsytem/FastManagementSystemApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class FastManagementSytemApplicationTests {
+class FastManagementSystemApplicationTests {
 
     @Test
     void contextLoads() {
