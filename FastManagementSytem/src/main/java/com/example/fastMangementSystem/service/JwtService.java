c684ecb3b7663cb4b8ccfad49c9d3ef2dce3871e
ermamatovsardor <ermamatovsardor0@gmail.com>
2023-06-08 17:41:49 +0500

    jwtService created

---
+++ FastManagementSytem/src/main/java/com/example/fastMangementSystem/service/JwtService.java
@@ -0,0 +1,48 @@
+package com.example.fastMangementSystem.service;
+
+import com.example.fastMangementSystem.entity.user.UserEntity;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.security.oauth2.resource.OAuth2ResourceServerProperties;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.stereotype.Service;
+
+import java.util.Collection;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+
+@Service
+public class JwtService {
+
+    @Value("${jwt.secret.key}")
+    private String secretKey;
+
+    @Value("${jwt.access.expiry}")
+    private long accessTokenExpiry;
+
+    public String generateAccessToken(UserEntity userEntity) {
+        return Jwts.builder()
+                .signWith(SignatureAlgorithm.HS512, secretKey)
+                .setSubject(userEntity.getEmail())
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(new Date().getTime() + accessTokenExpiry))
+                .addClaims(Map.of("roles", getRoles(userEntity.getAuthorities())))
+                .compact();
+    }
+
+    public Jws<Claims> extractToken(String token) {
+        return Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token);
+    }
+
+    private List<String> getRoles(Collection<? extends GrantedAuthority> roles) {
+        return roles.stream()
+                .map(GrantedAuthority::getAuthority)
+                .toList();
+    }
+}
+
+
