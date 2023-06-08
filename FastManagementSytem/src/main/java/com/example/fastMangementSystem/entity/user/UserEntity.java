e8ff1f1879d0ed13a277d7bc403df1e1ed27f001
javohirjahonov <jahonovjavohir0@gmail.com>
2023-06-08 17:18:29 +0500

    Added userDetails and role to user userEntity

---
+++ FastManagementSytem/src/main/java/com/example/fastMangementSystem/entity/user/UserEntity.java
@@ -0,0 +1,70 @@
+package com.example.fastMangementSystem.entity.user;
+
+import com.example.fastMangementSystem.entity.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+//@Getter
+//@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class UserEntity extends BaseEntity implements UserDetails {
+    private String name;
+    @Column(unique = true, nullable = false)
+    private String email;
+    @Column(unique = true, nullable = false)
+    private String phoneNumber;
+    private String password;
+    @Enumerated(EnumType.STRING)
+    private List<UserRole> roles;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        String ROLE="ROLE_";
+        List<SimpleGrantedAuthority> authorities=new ArrayList<>();
+        for (UserRole role : roles) {
+            authorities.add(new SimpleGrantedAuthority(ROLE + role.name()));
+        }
+        return authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return phoneNumber;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
