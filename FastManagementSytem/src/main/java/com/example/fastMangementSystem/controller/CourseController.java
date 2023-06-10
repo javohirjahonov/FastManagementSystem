eb43e5b5a0fa880c218e9e512c575477794e7cd9
Msi Laptop <hh@gg.com>
2023-06-10 19:34:57 +0500

    added CourseController

---
+++ FastManagementSytem/src/main/java/com/example/fastMangementSystem/controller/CourseController.java
@@ -0,0 +1,53 @@
+package com.example.fastMangementSystem.controller;
+
+import com.example.fastMangementSystem.dto.course.CourseCreatedDto;
+import com.example.fastMangementSystem.entity.course.CourseEntity;
+import com.example.fastMangementSystem.service.course.CourseService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/course/v1")
+@RequiredArgsConstructor
+public class CourseController {
+    private final CourseService courseService;
+    @PostMapping("/add")
+    public ResponseEntity<CourseEntity> add(
+            @RequestBody CourseCreatedDto courseCreatedDto,
+            @RequestParam UUID userId
+            ){
+        return ResponseEntity.ok(courseService.add(courseCreatedDto,userId));
+    }
+
+    @DeleteMapping("/delete")
+    public void delete(
+            @RequestParam UUID id
+    ){
+         courseService.deleteById(id);
+    }
+
+    @GetMapping("/get-all")
+    public ResponseEntity<List<CourseEntity>> getAll(
+            @RequestParam int size,
+            @RequestParam int page,
+            @RequestParam UUID userId
+    ){
+        return ResponseEntity.ok(courseService.getUserCourse(size, page, userId));
+    }
+
+    @PatchMapping("/{courseId}/update")
+    public ResponseEntity<CourseEntity> updateCourse(
+            @RequestBody CourseCreatedDto courseCreatedDto,
+            @PathVariable UUID courseId) {
+        return ResponseEntity.ok(courseService.update(courseCreatedDto, courseId));
+    }
+
+
+
+
+
+}
