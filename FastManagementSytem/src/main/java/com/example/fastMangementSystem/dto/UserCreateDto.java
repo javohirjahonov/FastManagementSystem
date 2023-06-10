4fe6bd5561ab5643f4a0058486c104eff2072cb2
Javokhir  Jakhonov <84447794+javohirjahonov@users.noreply.github.com>
2023-06-10 17:30:26 +0500

    Update UserCreateDto.java

--- FastManagementSytem/src/main/java/com/example/fastMangementSystem/dto/UserCreateDto.java
+++ FastManagementSytem/src/main/java/com/example/fastMangementSystem/dto/UserCreateDto.java
@@ -1,5 +1,7 @@
 package com.example.fastMangementSystem.dto;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
 import lombok.*;
 
 @Getter
@@ -8,7 +10,15 @@ import lombok.*;
 @NoArgsConstructor
 @Builder
 public class UserCreateDto {
+
+    @NotEmpty(message = "Name must not be empty")
     private String name;
+
+    @NotEmpty(message = " PhoneNumber must not be empty and only 7 numbers ")
+    @Pattern(regexp = "^\\d{9}$")
     private String phoneNumber;
+
+    @Pattern(regexp = "^(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*\\W).+$")
+    @NotEmpty(message = "Password must not be empty")
     private String password;
 }
