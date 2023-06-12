ba4160dbf740fe075f1c1d3376c43404dfc2b1f8
javohirjahonov <jahonovjavohir0@gmail.com>
2023-06-12 17:34:46 +0500

    Added Validation in Auth controller

--- FastManagementSytem/src/main/java/com/example/fastMangementSystem/controller/AuthController.java
+++ FastManagementSytem/src/main/java/com/example/fastMangementSystem/controller/AuthController.java
@@ -5,9 +5,12 @@ import com.example.fastMangementSystem.dto.UserCreateDto;
 import com.example.fastMangementSystem.dto.response.JwtResponse;
 import com.example.fastMangementSystem.entity.user.UserEntity;
 import com.example.fastMangementSystem.entity.user.UserRole;
+import com.example.fastMangementSystem.exception.RequestValidationException;
 import com.example.fastMangementSystem.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
@@ -19,27 +22,54 @@ public class AuthController {
 
     @GetMapping("/login")
     public ResponseEntity<JwtResponse> login(
-            @RequestBody LoginDto login
+            @RequestBody LoginDto login,
+            BindingResult bindingResult
     ) {
+
+        if (bindingResult.hasErrors()) {
+            List<ObjectError> allErrors = bindingResult.getAllErrors();
+            throw new RequestValidationException(allErrors);
+        }
         return ResponseEntity.ok(userService.login(login));
     }
     @PostMapping("/admin/sign-up")
     public ResponseEntity<UserEntity> adminSignUp(
-            @RequestBody UserCreateDto userDto
+            @RequestBody UserCreateDto userDto,
+            BindingResult bindingResult
     ) {
+
+
+        if (bindingResult.hasErrors()) {
+            List<ObjectError> allErrors = bindingResult.getAllErrors();
+            throw new RequestValidationException(allErrors);
+        }
         return ResponseEntity.ok(userService.save(userDto, List.of(UserRole.ADMIN)));
     }
 
     @PostMapping("/client/sign-up")
     public ResponseEntity<UserEntity> userSignUp(
-            @RequestBody UserCreateDto userDto
+            @RequestBody UserCreateDto userDto,
+            BindingResult bindingResult
     ) {
+
+
+        if (bindingResult.hasErrors()) {
+            List<ObjectError> allErrors = bindingResult.getAllErrors();
+            throw new RequestValidationException(allErrors);
+        }
         return ResponseEntity.ok(userService.save(userDto, List.of(UserRole.CLIENT)));
     }
     @PostMapping("/mentor/sign-up")
     public ResponseEntity<UserEntity> mentorSignUp(
-            @RequestBody UserCreateDto userDto
+            @RequestBody UserCreateDto userDto,
+            BindingResult bindingResult
     ) {
+
+
+        if (bindingResult.hasErrors()) {
+            List<ObjectError> allErrors = bindingResult.getAllErrors();
+            throw new RequestValidationException(allErrors);
+        }
         return ResponseEntity.ok(userService.save(userDto, List.of(UserRole.MENTOR)));
     }
     @PostMapping("/operator/sign-up")
