1e1ac8f72ce9ca281c024442ea6908f04da6ab3c
abduvohidovismoil <abduvohidovi72@gmail.com>
2023-06-14 10:39:47 +0500

    added update to Order Service

--- FastManagementSytem/src/main/java/com/example/fastMangementSystem/service/orderService/OrderService.java
+++ FastManagementSytem/src/main/java/com/example/fastMangementSystem/service/orderService/OrderService.java
@@ -9,9 +9,7 @@ import com.example.fastMangementSystem.repository.course.CourseRepository;
 import com.example.fastMangementSystem.repository.orderRepository.OrderRepository;
 import com.example.fastMangementSystem.repository.user.UserRepository;
 import lombok.RequiredArgsConstructor;
-import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
-
 import java.util.List;
 import java.util.UUID;
 
@@ -42,8 +40,10 @@ public class OrderService {
         return orderEntity;
     }
 
-    public OrderEntity update(OrderEntity orderEntity) {
-        return null;
+    public OrderEntity update(UUID orderId) {
+        OrderEntity order = orderRepository.findById(orderId)
+                .orElseThrow(() -> new DataNotFoundException("Order not found"));
+        return orderRepository.save(order);
     }
 
     public void delete(UUID id) {
@@ -51,8 +51,7 @@ public class OrderService {
     }
 
     public OrderEntity getById(UUID id) {
-        OrderEntity order = orderRepository.findById(id)
+        return orderRepository.findById(id)
                 .orElseThrow(() -> new DataNotFoundException("This order not found"));
-        return order;
     }
 }
